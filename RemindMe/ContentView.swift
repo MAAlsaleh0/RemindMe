6d54e0280a018de9b89177e0e553b3275423d740
Mohammed Alsaleh <Malsalehh0@gmail.com>
2022-04-17 15:33:28 -0700

    Initial Commit

---
+++ RemindMe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RemindMe
+//
+//  Created by Mohammed Alsaleh on 16/09/1443 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RemindMe/RemindMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindMeApp.swift
+//  RemindMe
+//
+//  Created by Mohammed Alsaleh on 16/09/1443 AH.
+//
+
+import SwiftUI
+
+@main
+struct RemindMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
